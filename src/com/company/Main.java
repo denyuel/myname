7e4342a844c71493678164bf65f81bb99a6f8823
denyuel <denyuel@gmail.com>
2018-09-13 10:23:25 +0200

    My name commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("My name is");
+        System.out.println("Daniel Borbas");
     }
 }
