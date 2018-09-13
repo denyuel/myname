9668c176187c8606eb7a892fe4a3ddd0c5796b34
denyuel <denyuel@gmail.com>
2018-09-13 15:34:19 +0200

    triangle commit

--- src/areaoftriangle.java
+++ src/areaoftriangle.java
@@ -5,16 +5,15 @@ public class areaoftriangle {
         public static void main(String args[])
         {
 
-            Scanner s= new Scanner(System.in);
+            Scanner triangle= new Scanner(System.in);
 
             System.out.println("Enter the width of the Triangle:");
-            double b= s.nextDouble();
+            int a= triangle.nextInt();
 
             System.out.println("Enter the height of the Triangle:");
-            double h= s.nextDouble();
+            int b= triangle.nextInt();
 
-            //Area = (width*height)/2
-            double area=(b*h)/2;
+            int area=(a*b)/2;
             System.out.println("Area of Triangle is: " + area);
         }
     }
