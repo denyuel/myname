3e2b20445061f59c56c3cf9d1e0e096eed9c0216
denyuel <denyuel@gmail.com>
2018-09-17 09:13:39 +0200

    triangle commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String mynamestr = new String ("My name is");
+        String mynamestr = new String ("My name is ");
         String myname = new String ("Daniel Borbas");
-        System.out.println(mynamestr);
-        System.out.println(myname);
+        System.out.print(mynamestr + myname);
     }
 }
