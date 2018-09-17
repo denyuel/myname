19bfbba97022d605e77f08f92cf43b4c8bf1c54c
denyuel <denyuel@gmail.com>
2018-09-17 16:15:09 +0200

    triangle commit

--- src/nowhitespace.java
+++ src/nowhitespace.java
@@ -1,7 +1,8 @@
 public class nowhitespace { ;
 
     public static void main(String[] args) {
-        String myname = new String ("Mynameisdanielborbas");
-        System.out.print(myname);
+        String text = new String ("My name is ");
+        String myName=new String("Daniel Borbas");
+        System.out.print(text + myName);
     }
 }
