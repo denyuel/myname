6e56f65eb555998bb1d224d54c5790d924e8894b
denyuel <denyuel@gmail.com>
2018-09-17 17:00:04 +0200

    triangle commit

---
+++ src/practice.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class practice {
+
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        double a= sc.nextDouble();
+        double b= sc.nextDouble();
+        System.out.println("Please enter"); //taglalap terulete
+
+
+    }
+
+
+}
