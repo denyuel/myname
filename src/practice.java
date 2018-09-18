b4b7e4df308ee0051a8895abf0c882853278be09
denyuel <denyuel@gmail.com>
2018-09-18 10:24:50 +0200

    triangle commit

--- src/practice.java
+++ src/practice.java
@@ -5,17 +5,24 @@ public class practice {
     public static void main(String[] args) {
 
         Scanner sc= new Scanner(System.in);
+
         System.out.println("Please enter a double");
         double a= sc.nextDouble();
         System.out.println("Please enter a double");
         double b= sc.nextDouble();
 
+        System.out.println("Please enter a float");
+        float c= sc.nextFloat();
+        System.out.println("Please enter a float");
+        float d= sc.nextFloat();
 
-        double area = a+b*2;
-        System.out.println("The result is: " + area); //taglalap terulete
+
+
+        double area1 = a+b*2;
+        float area2 = c+d*2;
+        System.out.println("The result is: " + area1 + area2); //taglalap terulete
 
 
     }
-
 
 }
