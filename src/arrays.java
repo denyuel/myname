44d8965e04b9780fb4acbc913b11e6ea41fce2d7
denyuel <denyuel@gmail.com>
2018-09-28 13:47:45 +0200

    triangle commit

---
+++ src/arrays.java
@@ -0,0 +1,16 @@
+public class arrays {
+
+        public static void main(String[] args) {
+            int[] a = new int[3];
+            a[0] = 1;
+            a[0] = 5;
+            a[1] = 2;
+            a[2] = 3;
+            char[] c = new char[2];
+            c[0]= 'f';
+            c[1]= 'g';
+            System.out.println(a[0]);
+            System.out.println(c[0]);
+            System.out.println(c[1]);
+        }
+    }
